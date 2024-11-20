672a257ceb412f543bd9066b4e3505a7167ade20
Anoob Suresh <anoobsuresh0@gmail.com>
2024-11-21 00:11:14 +0530

    fix menu text color

--- AppraisalSystem/AppraisalSystemFrontend/appraisal_system/src/components/Header.tsx
+++ AppraisalSystem/AppraisalSystemFrontend/appraisal_system/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="w-full  bg-white p-4 flex justify-between items-center shadow">
-      <span className="text-lg font-bold ml-4">Menu</span>
+      <span className="text-lg text-black font-bold ml-4">Menu</span>
       
     </header>
   )
